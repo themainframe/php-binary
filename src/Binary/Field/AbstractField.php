964bdc29fb066fb76056c6a3a1b72b9734da0da6
Damien Walsh <me@damow.net>
2014-01-15 20:58:10 +0000

    Added basic validation.

--- src/Binary/Field/AbstractField.php
+++ src/Binary/Field/AbstractField.php
@@ -8,6 +8,8 @@
  */
 namespace Binary\Field;
 
+use Binary\Validator\AbstractValidator;
+
 /**
  * Field
  * Abstract.
@@ -17,7 +19,32 @@ namespace Binary\Field;
 abstract class AbstractField implements FieldInterface
 {
     /**
-     * @public string $name The name of the field.
+     * @public Binary\AbstractValidator[] Validators attached to the field.
+     */
+    private $validators = array();
+
+    /**
+     * @public string The name of the field.
      */
     public $name = '';
+
+    /**
+     * Attach a validator to the field.
+     *
+     * @param AbstractValidator $validator
+     */
+    public function addValidator(AbstractValidator $validator)
+    {
+        $this->validators[] = $validator;
+    }
+
+    /**
+     * Check that the field is currently valid.
+     */
+    public function validate($value)
+    {
+        foreach ($this->validators as $validator) {
+            $validator->validate($value);
+        }
+    }
 }
