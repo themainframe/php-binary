646db711c7c699282eb42a2d9dcebea6ac7d058a
Damien Walsh <damienw@magmadigital.co.uk>
2014-04-29 16:49:54 +0100

    Added an exception for invalid schema constructions.

---
+++ src/Exception/SchemaException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * php-binary
+ * A PHP library for parsing structured binary streams
+ *
+ * @package  php-binary
+ * @author Damien Walsh <me@damow.net>
+ */
+namespace Binary\Exception;
+
+/**
+ * SchemaException
+ *
+ * @since 1.0
+ */
+class SchemaException extends \Exception
+{
+}
