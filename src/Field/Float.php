5629ad021a795a3dba3dcda5b46bc36b71b6089b
Shane Egan <egnshane@gmail.com>
2015-08-19 21:40:55 +0100

    Added: Float type has been added

---
+++ src/Field/Float.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * php-binary
+ * A PHP library for parsing structured binary streams
+ *
+ * @package  php-binary
+ * @author Damien Walsh <me@damow.net>
+ */
+namespace Binary\Field;
+
+use Binary\DataSet;
+use Binary\Stream\StreamInterface;
+
+/**
+ * Float
+ * Field.
+ *
+ * @since 1.0
+ */
+class Float extends AbstractSizedField
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function read(StreamInterface $stream, DataSet $result)
+    {
+        $data = $stream->read($this->size->get($result));
+
+        if (strlen($data) < 2) {
+            $data = str_pad($data, 2, "\0", STR_PAD_LEFT);
+        }
+
+        $unpacked = unpack('f', $data);
+        $this->validate($unpacked[1]);
+        $result->setValue($this->name, $unpacked[1]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write(StreamInterface $stream, DataSet $result)
+    {
+        //Writing floats currently untested
+        $bytes = $result->getValue($this->name);
+        $stream->write(pack('f', intval($bytes)));
+    }
+}
